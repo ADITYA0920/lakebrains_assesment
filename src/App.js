ca9269ee3df745615cac4185b7dd76e26544b0c8
ADITYA0920 <adityamane21880@gmail.com>
2023-11-06 12:17:16 +0530

    upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useMemo, useState } from 'react'
+import "./App.css" 
 
-function App() {
+const App = () => {
+  let [search,setSearch] = useState("") ;
+  let [data,setData] = useState({
+    name:"",
+    temp:"",
+    main:"",
+  })
+  const wipeData = ()=>{
+    setSearch("") ;
+  }
+  const searchData = ()=>{
+    let popup = document.querySelector('.popup-message') ;
+    popup.style.disply = 'block';
+  }
+  const fetchData = async(e)=>{
+   e.preventDefault() ;
+   let Key =  "840de593b7028de6e424162454790fe5"
+   let url =  `https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=${Key}`
+
+   let data = await fetch(url) ;
+   let res = await data.json();
+
+   console.log(res) ;
+
+   setData({
+     name : res.name,
+     temp :res.main.temp ,
+     main : res.clouds
+   }) 
+  }
+  // let memo = useMemo(fetchData,search) ;
+
+  const cancelBtnClick = ()=>{
+    let popup = document.querySelector('.popup-message') ;
+    popup.style.disply = 'none';
+  }
+  // useEffect( async() => {
+    
+  // },search)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <form onSubmit={fetchData}>
+        <div>
+        <input type='text' placeholder='search' value={search} onChange={(e)=>setSearch(e.target.value)}/>
+        <button onClick={wipeData}>x</button>
+        </div>
+        <button onClick={searchData}>search</button>
+      </form>
+      <div className='popup'>
+        {
+          data && (
+            <div className='popup-message'>
+              <div className='container2'>
+                <h3>name : {data.name}</h3>
+                <h3>temp : {data.temp}</h3>
+                <h3>main : {data.main}</h3>
+              </div>
+              <button id='cancel-button' onClick={cancelBtnClick}>x</button>
+            </div>
+          )          
+        }
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App ;
